7c7231788642bb8e030896c1f89db4eafbed09b0
Yasir Ali <yasirali1234@gmail.com>
2015-08-28 22:54:57 +0300

    no message

---
+++ MovieList/app/src/main/java/com/a1401358/model/Movie.java
@@ -0,0 +1,12 @@
+package com.a1401358.model;
+/**
+ * Created by Imran on 8/28/2015.
+ */
+public class Movie {
+    public String movieId;
+    public String thumbnailUrl;
+    public String title;
+    public String director;
+    public String genres;
+    public String actors;
+}
--- MovieList/app/src/main/java/com/a1401358/movielist/MovieListAdapater.java
+++ MovieList/app/src/main/java/com/a1401358/movielist/MovieListAdapater.java
@@ -5,13 +5,21 @@ import android.view.View;
 import android.view.ViewGroup;
 import android.widget.BaseAdapter;
 
+import com.a1401358.model.Movie;
+
+import java.util.ArrayList;
+import java.util.List;
+
 /**
  * Created by Imran on 8/28/2015.
  */
 public class MovieListAdapater extends BaseAdapter {
+
+    private List<Movie> movieList = new ArrayList<>();
+
     @Override
     public int getCount() {
-        return 20;
+        return movieList.size();
     }
 
     @Override
@@ -26,9 +34,8 @@ public class MovieListAdapater extends BaseAdapter {
 
     @Override
     public View getView(int position, View convertView, ViewGroup parent) {
-
-        if(convertView== null){
-            convertView = LayoutInflater.from(parent.getContext()).inflate(R.layout.movie_item_layout,parent,false);
+        if (convertView == null) {
+            convertView = LayoutInflater.from(parent.getContext()).inflate(R.layout.movie_item_layout, parent, false);
         }
         return convertView;
     }
